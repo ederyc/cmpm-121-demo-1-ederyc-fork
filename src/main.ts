5bb27d8a63ad52ae30b7eb93ca4565eb1724e5f1
ederyc <edyepezc@ucsc.edu>
2024-10-09 12:30:49 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -11,23 +11,29 @@ app.append(header);
 
 //add button incrementer
 let click_count: number = 0;
+let previous: number = performance.now();
 
 const button = document.querySelector(".favorite") as HTMLButtonElement;
 
-//amount was only being displayed when button was 
+//amount was only being displayed when button was
 //clicked so this now constantly updates the amount of units being displayed
 function updateButtonText() {
-  button.textContent = `😏 Oh My x ${click_count}`;
+  button.textContent = `😏 Oh My x ${click_count.toFixed(2)}`;
 }
 
 button.addEventListener("click", () => {
-  click_count++
+  click_count++;
   updateButtonText();
 });
 
-function interval_incr() {
-  click_count++;
+function interval_incr(current: number) {
+  const diff = (current - previous) / 1000;
+  click_count += diff;
   updateButtonText();
+  previous = current;
+
+  requestAnimationFrame(interval_incr);
 }
 
-setInterval(interval_incr, 1000);
+requestAnimationFrame(interval_incr);
+
