daffb7d55f7c6a1ec97babe94134f77c3489e251
ederyc <edyepezc@ucsc.edu>
2024-10-09 10:03:26 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,20 @@ let click_count: number = 0;
 
 const button = document.querySelector(".favorite") as HTMLButtonElement;
 
-button.addEventListener("click", () => {
-  click_count++;
+//amount was only being displayed when button was 
+//clicked so this now constantly updates the amount of units being displayed
+function updateButtonText() {
   button.textContent = `😏 Oh My x ${click_count}`;
+}
+
+button.addEventListener("click", () => {
+  click_count++
+  updateButtonText();
 });
+
+function interval_incr() {
+  click_count++;
+  updateButtonText();
+}
+
+setInterval(interval_incr, 1000);
