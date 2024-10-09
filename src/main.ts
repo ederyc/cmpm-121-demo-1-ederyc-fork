85b9408d63301b34de8af7fc93196c69b08e91b5
ederyc <edyepezc@ucsc.edu>
2024-10-09 09:48:48 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,14 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//add button incrementer
+let click_count: number = 0;
+
+const button = document.querySelector('.favorite') as HTMLButtonElement;
+
+button.addEventListener('click', () => {
+  click_count++;
+  button.textContent = `😏 Oh My x ${click_count}`;
+});
+
